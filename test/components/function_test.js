195c549ef0bd4ab454ee5ecf6f6844347b69b056
Kirill Matrosov <kdmatrosov@gmail.com>
2017-01-09 15:33:57 +0300

    test. describe('testAction')

--- test/components/function_test.js
+++ test/components/function_test.js
@@ -1,3 +1,10 @@
-/**
- * Created by root on 1/9/17.
- */
+import { expect } from '../test_helper';
+import {testAction} from '../../src/actions/index';
+
+describe('testAction' , () => {
+
+    it('testAction is working', () => {
+        const res = testAction();
+        expect(res.payload).to.equal(true);
+    });
+});
