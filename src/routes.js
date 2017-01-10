d08c1016bec2797b70a2ab5c8a83c163458db9ba
Kirill Matrosov <kdmatrosov@gmail.com>
2017-01-10 19:25:28 +0300

    add standard to project

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import {Route, IndexRoute} from 'react-router';
-import App from './components/app';
-import Test from './containers/test';
-import PhotoIndex from './containers/photo/photo_index';
+import React from 'react'
+import {Route, IndexRoute} from 'react-router'
+import App from './components/app'
+import Test from './containers/test'
+import PhotoIndex from './containers/photo/photo_index'
 
 export default (
-    <Route path="/" component={App}>
-        <IndexRoute component={Test}>
-
-        </IndexRoute>
-        <Route path="photographers" component={PhotoIndex}></Route>
-    </Route>
-);
+  <Route path='/' component={App}>
+    <IndexRoute component={Test} />
+    <Route path='photographers' component={PhotoIndex} />
+  </Route>
+)
