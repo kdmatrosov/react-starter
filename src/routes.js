307548f4fafd45e823f9e8af045f15f4097e9d21
Kirill Matrosov <kdmatrosov@gmail.com>
2017-01-10 11:39:08 +0300

    containers add

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import App from './components/app';
-import Test from './components/test';
+import Test from './containers/test';
 
 export default (
     <Route path="/" component={App}>
