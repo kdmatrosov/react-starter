f7422454b76c63cf423844320b594e31aed041be
Kirill Matrosov <kdmatrosov@gmail.com>
2017-02-01 12:13:36 +0300

    postcss.config update

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   plugins: {
-    'precss': {},
-    'postcss-import': {},
     'postcss-cssnext': {
       browsers: ['last 2 versions', '> 5%']
-    }
+    },
+    'precss': {},
+    'postcss-simple-vars': {},
+    'postcss-mixins': {},
+    'postcss-import': {}
   }
 };
